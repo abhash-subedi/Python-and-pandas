df2d8cea5962c016411b7427c6cb1570e7794128
a314159265 <abhash.subedi@renegadeinsurance.com>
2026-07-13 12:51:27 +0545

    first commit

--- mod7.py
+++ mod7.py
@@ -1,1 +1,16 @@
-print('hello world')
+import pandas as pd
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+FILE_PATH = ROOT_DIR / "data" / "titanic.csv"
+
+def load_titanic_data(file_path: Path = FILE_PATH) -> pd.DataFrame:
+    """
+    Load the Titanic dataset from a CSV file.
+
+    Args:
+        file_path (Path): The path to the CSV file. Defaults to FILE_PATH.
+    Returns:
+        pd.DataFrame: A DataFrame containing the Titanic dataset.
+    """
+    return pd.read_csv(file_path)
---
+++ python_task_1/task_1.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+FILE_PATH = ROOT_DIR / "data" / "titanic.csv"
